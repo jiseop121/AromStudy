bcebf157e4ec5cebb3122ddd7f4690a537abf392
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-04 01:03:45 +0900

    Feat : first init

---
+++ src/test/java/arom/springtoy/SpringtoyApplicationTests.java
@@ -0,0 +1,13 @@
+package arom.springtoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
