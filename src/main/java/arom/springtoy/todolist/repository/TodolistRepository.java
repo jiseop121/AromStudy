e2286d121ddde3201544d89c1de1c31771c29e65
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-07 02:02:08 +0900

    Feat : TodolistRepository생성

--- src/main/java/arom/springtoy/todolist/repository/TodolistRepository.java
+++ src/main/java/arom/springtoy/todolist/repository/TodolistRepository.java
@@ -1,5 +1,10 @@
 package arom.springtoy.todolist.repository;
 
-public interface TodolistRepository {
+import arom.springtoy.todolist.domain.Todolist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodolistRepository extends JpaRepository<Todolist, Long> {
 
 }
