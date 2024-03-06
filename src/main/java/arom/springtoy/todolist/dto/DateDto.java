372920f251a452f4acb07f1671a22e81a3356403
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-07 02:01:49 +0900

    Feat : DateDto validation 추가

--- src/main/java/arom/springtoy/todolist/dto/DateDto.java
+++ src/main/java/arom/springtoy/todolist/dto/DateDto.java
@@ -1,14 +1,32 @@
 package arom.springtoy.todolist.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @RequiredArgsConstructor
 public class DateDto {
+
+    @NotNull @NotBlank
+    @Range(min = 2024, max = 2080)
     private final int year;
+
+    @NotNull @NotBlank
+    @Range(min = 1, max = 12)
     private final int month;
+
+    @NotNull @NotBlank
+    @Range(min = 1, max = 31)
     private final int dayOfMonth;
+
+    @NotNull @NotBlank
+    @Range(min = 0, max = 23)
     private final int hour;
+
+    @NotNull @NotBlank
+    @Range(min = 0, max = 59)
     private final int minute;
 }
