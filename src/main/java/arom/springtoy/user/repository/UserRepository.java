1267ae2e6970211bbfe5534114c29cd97336d877
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-07 02:02:47 +0900

    Feat : UserRepository 생성

--- src/main/java/arom/springtoy/user/repository/UserRepository.java
+++ src/main/java/arom/springtoy/user/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package arom.springtoy.user.repository;
 
-public interface UserRepository {
+import arom.springtoy.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
