44ec03cbee9d3e2ca828cc859cd577511a7b2754
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-07 02:01:28 +0900

    Feat : (ContentRepository) 생성

---
+++ src/main/java/arom/springtoy/content/repository/ContentRepository.java
@@ -0,0 +1,10 @@
+package arom.springtoy.content.repository;
+
+import arom.springtoy.content.domain.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContentRepository extends JpaRepository<Content,Long> {
+
+}
