73a19b9e4865bb7ec41a392e2c81fb3df7d71a7d
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-11 01:01:48 +0900

    Feat : todolistRepository 메서드 추가

--- src/main/java/arom/springtoy/todolist/repository/TodolistRepository.java
+++ src/main/java/arom/springtoy/todolist/repository/TodolistRepository.java
@@ -1,10 +1,23 @@
 package arom.springtoy.todolist.repository;
 
 import arom.springtoy.todolist.domain.Todolist;
+import arom.springtoy.user.domain.User;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodolistRepository extends JpaRepository<Todolist, Long> {
+
+    Optional<Todolist> findByTodoListName(String todolistName);
+
+    Optional<Todolist> findByTodoListNameAndUser(String todolistName, User user);
+
+    List<Todolist> findAllByUser(User user);
+
+    void deleteByTodolistId(Long todolistId);
+
+    Optional<Todolist> findByTodolistIdAndUser(Long todolistId, User user);
 
 }
