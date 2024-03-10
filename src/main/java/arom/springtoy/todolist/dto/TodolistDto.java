6d3d63890956ba4db49650b46235216dde234210
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-11 00:57:32 +0900

    Feat : todolistDto 추가용 dto 생성

--- src/main/java/arom/springtoy/todolist/dto/TodolistDto.java
+++ src/main/java/arom/springtoy/todolist/dto/TodolistDto.java
@@ -1,5 +1,26 @@
 package arom.springtoy.todolist.dto;
 
-public class TodolistDto {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodolistDto {
+    @NotNull @NotBlank
+    private String todolistName;
+
+    @NotNull @NotBlank
+    @Size(min = 1, max = 30)
+    private String writer;
+
+    @NotNull
+    private DateDto startDate;
+
+    @NotNull
+    private DateDto endDate;
 }
