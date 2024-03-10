e89e5fbc4632fdef25b842defc83256cc4f485af
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-11 00:58:15 +0900

    Refactor : Datedto 수정

--- src/main/java/arom/springtoy/todolist/dto/DateDto.java
+++ src/main/java/arom/springtoy/todolist/dto/DateDto.java
@@ -2,31 +2,36 @@ package arom.springtoy.todolist.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
 
 @Getter
-@RequiredArgsConstructor
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DateDto {
 
     @NotNull @NotBlank
     @Range(min = 2024, max = 2080)
-    private final int year;
+    private int year;
 
     @NotNull @NotBlank
     @Range(min = 1, max = 12)
-    private final int month;
+    private int month;
 
     @NotNull @NotBlank
     @Range(min = 1, max = 31)
-    private final int dayOfMonth;
+    private int dayOfMonth;
 
     @NotNull @NotBlank
     @Range(min = 0, max = 23)
-    private final int hour;
+    private int hour;
 
     @NotNull @NotBlank
     @Range(min = 0, max = 59)
-    private final int minute;
+    private int minute;
 }
