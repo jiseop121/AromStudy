0b41fa57a6ef4e6707ff78633e530e3085cc36f9
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-13 10:19:41 +0900

    Feat : Controller별 exception 생성

---
+++ src/main/java/arom/springtoy/domain/exception/ContentException.java
@@ -0,0 +1,11 @@
+package arom.springtoy.domain.exception;
+
+public class ContentException extends RuntimeException {
+
+    public ContentException() {
+    }
+
+    public ContentException(String message) {
+        super(message);
+    }
+}
