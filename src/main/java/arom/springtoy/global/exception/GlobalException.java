77bac7f765584a11ca0e76ccc73c71bc9545a83c
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-13 10:16:08 +0900

    Feat : 서비스 단에 적용할 Exception 생성

---
+++ src/main/java/arom/springtoy/global/exception/GlobalException.java
@@ -0,0 +1,11 @@
+package arom.springtoy.global.exception;
+
+public class GlobalException extends RuntimeException{
+
+    public GlobalException() {
+    }
+
+    public GlobalException(String message) {
+        super(message);
+    }
+}
